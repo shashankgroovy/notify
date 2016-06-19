4dd88d44cb44a1edc1c897964f3a99339acf0db3
Shashank Srivastav <shashankgrovy@gmail.com>
2016-06-19 17:44:39 +0530

    Use jquery and add favicon

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,7 @@
-function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if(e.style.display == 'block')
-        e.style.display = 'none';
-    else
-        e.style.display = 'block';
-}
+// Go Vanilla
 
-function toggleNotifications() {
-    toggle_visibility( 'noti-container' );
-}
+$(document).ready(function(){
+    $("#noti-box").click(function(){
+        $("#noti-container").fadeToggle("slow");
+    });
+});
